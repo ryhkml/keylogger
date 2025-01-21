7d9c313e8d572b174b095e87b9212c1ea429dcfb
ryhkml <reyhank95@hotmail.com>
2025-01-21 23:37:58 +0700

    Enhance server initialization with security and add SSL configuration options

--- websocket.c
+++ websocket.c
@@ -1,6 +1,7 @@
 #include "websocket.h"
 
 #include <libwebsockets.h>
+#include <stdbool.h>
 
 struct lws_context *context;
 struct lws *client_wsi = NULL;
@@ -55,12 +56,38 @@ struct lws_protocols protocols[] = {{
                                     {NULL, NULL, 0, 0, .id = 0}};
 
 int init_websocket_server(int port) {
+    if (port < 4000 || port > 65535) {
+        perror("Use ports in the range 4000 to 65535\n");
+        return -1;
+    }
+
     struct lws_context_creation_info info;
     memset(&info, 0, sizeof(info));
     info.port = port;
     info.protocols = protocols;
     info.gid = -1;
     info.uid = -1;
+    info.options |= LWS_SERVER_OPTION_HTTP_HEADERS_SECURITY_BEST_PRACTICES_ENFORCE;
+    info.options |= LWS_SERVER_OPTION_VALIDATE_UTF8;
+    info.options |= LWS_SERVER_OPTION_DISABLE_IPV6;
+
+    const char *env_ssl_cert = getenv("KEYLOGGER_SSL_CERT_PATH");
+    const char *env_ssl_key = getenv("KEYLOGGER_SSL_KEY_PATH");
+    const bool ssl_cert_path = env_ssl_cert != NULL && env_ssl_cert[0] != '\0';
+    const bool ssl_key_path = env_ssl_key != NULL && env_ssl_key[0] != '\0';
+
+    if (ssl_cert_path && ssl_key_path) {
+        printf("Use secure websocket\n");
+        info.options |= LWS_SERVER_OPTION_DO_SSL_GLOBAL_INIT;
+        info.ssl_cert_filepath = env_ssl_cert;
+        info.ssl_private_key_filepath = env_ssl_key;
+        // Optional CA
+        const char *env_ssl_ca = getenv("KEYLOGGER_SSL_CA_PATH");
+        const bool ssl_ca_path = env_ssl_ca != NULL && env_ssl_ca[0] != '\0';
+        if (ssl_ca_path) {
+            info.ssl_ca_filepath = env_ssl_ca;
+        }
+    }
 
     static const struct lws_protocol_vhost_options headers[] = {{NULL, NULL, "Cache-Control", "no-cache, no-store"},
                                                                 {NULL, NULL, NULL, NULL}};
