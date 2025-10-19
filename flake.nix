089a013d09d6be1ef59d48c1a34eb76694138b10
ryhkml <ryhkmll@gmail.com>
2025-10-19 12:51:43 +0700

    Build(nix): always build with websockets

--- flake.nix
+++ flake.nix
@@ -17,36 +17,34 @@
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        mkPackage =
-          websockets:
-          pkgs.stdenv.mkDerivation {
-            name = "keylogger";
-            src = pkgs.lib.cleanSourceWith {
-              src = ./.;
-              filter = path: type: !(pkgs.lib.hasSuffix ".gif" path || pkgs.lib.hasSuffix ".png" path || pkgs.lib.hasSuffix ".txt" path);
-            };
-            nativeBuildInputs =
-              [
-                pkgs.gcc
-              ]
-              ++ (pkgs.lib.optional websockets [
-                pkgs.libwebsockets.dev
-                pkgs.openssl.dev
-              ]);
-            buildPhase = ''
-              gcc -o nob nob.c
-              ./nob ${if websockets then "-lwebsockets" else ""}
-            '';
-            installPhase = ''
-              mkdir -p $out/bin
-              cp out/keylogger $out/bin/
-            '';
+        mkPackage = pkgs.stdenv.mkDerivation {
+          name = "keylogger";
+          src = pkgs.lib.cleanSourceWith {
+            src = ./.;
+            filter =
+              path: type:
+              !(
+                pkgs.lib.hasSuffix ".mp4" path || pkgs.lib.hasSuffix ".png" path || pkgs.lib.hasSuffix ".txt" path
+              );
           };
+          nativeBuildInputs = [
+            pkgs.gcc
+            pkgs.libwebsockets.dev
+            pkgs.openssl.dev
+          ];
+          buildPhase = ''
+            gcc -o nob nob.c
+            ./nob
+          '';
+          installPhase = ''
+            mkdir -p $out/bin
+            cp out/keylogger $out/bin/
+          '';
+        };
       in
       {
         packages = {
-          default = mkPackage false;
-          with-websockets = mkPackage true;
+          default = mkPackage;
         };
         apps = {
           default = {
@@ -55,16 +53,6 @@
               let
                 wrapper = pkgs.writeShellScriptBin "keylogger" ''
                   ${self.packages.${system}.default}/bin/keylogger "$@"
-                '';
-              in
-              "${wrapper}/bin/keylogger";
-          };
-          with-websockets = {
-            type = "app";
-            program =
-              let
-                wrapper = pkgs.writeShellScriptBin "keylogger" ''
-                  ${self.packages.${system}.with-websockets}/bin/keylogger "$@"
                 '';
               in
               "${wrapper}/bin/keylogger";
@@ -78,9 +66,6 @@
           ];
           shellHook = ''
             echo "Keylogger development shell"
-            echo "Build options:"
-            echo "- Default build: nix build"
-            echo "- Build with websocket: nix build .#with-websockets"
           '';
         };
       }
